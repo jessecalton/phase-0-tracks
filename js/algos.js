24ce9cb4def6e2764139bc7a3686356d9cae8537
Jesse Calton <jessecalton@gmail.com>
2017-01-28 22:54:21 -0800

    finished release 1

--- js/algos.js
+++ js/algos.js
@@ -13,22 +13,65 @@
 // Use that index location and fetch the value from our
 // original array.
 
-var longArray = ["long array", "longer array", "longest array"];
-var newArray = ["french fries", "oreos", "Canadian bacon", "gumballs"];
+// var longArray = ["long array", "longer array", "longest array"];
+// var newArray = ["french fries", "oreos", "Canadian bacon", "gumballs"];
 
-function countLength(arr) {
-  var biggestNum = 0
-  var longestName = ""
-  for (i = 0; i <= arr.length - 1; i++) {
-    if (arr[i].length > biggestNum) {
-      biggestNum = arr[i].length;
-      longestName = arr[i]
-      }
+// function countLength(arr) {
+//   var biggestNum = 0
+//   var longestName = ""
+//   for (i = 0; i <= arr.length - 1; i++) {
+//     if (arr[i].length > biggestNum) {
+//       biggestNum = arr[i].length;
+//       longestName = arr[i]
+//       }
       
-    }
-  console.log(longestName)
+//     }
+//   console.log(longestName)
+// }
+
+// countLength(longArray);
+
+// countLength(newArray);
+
+var hashMike = { name: "Mike", age: "26" }
+var hashRoger = { name: "Roger", age: "32" }
+var hashEd = { name: "Ed", age: "26" }
+
+console.log(hashMike["age"] == hashEd["age"]);
+
+function compareHashes(hash1, hash2){
+  console.log(hash1);
+  console.log(hash2);
+  if (hash1["age"] == hash2["age"]){
+  return true;
+}  else if (hash1["name"] == hash2["name"]){
+  return true;
+}  else {
+  return false
+}
 }
 
-countLength(longArray);
+compareHashes(hashMike, hashEd)
 
-countLength(newArray);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
