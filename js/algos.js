d0f1519b5c1f9c41d2d136e006d9ba818acc4876
Jesse Calton <jessecalton@gmail.com>
2017-01-28 21:49:49 -0800

    finished release 0

--- js/algos.js
+++ js/algos.js
@@ -13,10 +13,22 @@
 // Use that index location and fetch the value from our
 // original array.
 
-var longArray = ["long array", "longer array", "longest array"]
+var longArray = ["long array", "longer array", "longest array"];
+var newArray = ["french fries", "oreos", "Canadian bacon", "gumballs"];
 
 function countLength(arr) {
-  for (var i = 0; i < i.length; i++) {
-    console.log(arr[i]);
-  }
+  var biggestNum = 0
+  var longestName = ""
+  for (i = 0; i <= arr.length - 1; i++) {
+    if (arr[i].length > biggestNum) {
+      biggestNum = arr[i].length;
+      longestName = arr[i]
+      }
+      
+    }
+  console.log(longestName)
 }
+
+countLength(longArray);
+
+countLength(newArray);
