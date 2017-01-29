8ad50101ba99ca07ea13bbbfd757119d2f237bc6
Jesse Calton <jessecalton@gmail.com>
2017-01-28 20:30:58 -0800

    initial commit

---
+++ js/algos.js
@@ -0,0 +1,22 @@
+// Write a function that takes an array of words or phrases
+// and returns the longest word or phrase in the array.
+
+// PSUEDOCODE!
+// Create an array of strings of differing lengths
+// Return the values of the length of each string in
+// the array.
+// Get the index location of the highest value
+//  Use a counter and keep counting up until it equals
+//  the highest value.
+//  Grab that value and use it to fetch the position
+//  of that value in the array of string lengths.
+// Use that index location and fetch the value from our
+// original array.
+
+var longArray = ["long array", "longer array", "longest array"]
+
+function countLength(arr) {
+  for (var i = 0; i < i.length; i++) {
+    console.log(arr[i]);
+  }
+}
