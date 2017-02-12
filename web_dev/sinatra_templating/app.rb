de99b4963c74c4fc2870d18417882d13d0ab080a
Jesse Calton <jessecalton@gmail.com>
2017-02-11 17:02:07 -0800

    finished 9.5

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -17,11 +17,37 @@ get '/students/new' do
   erb :new_student
 end
 
+get '/advisors' do 
+  erb :advisors
+end 
+
 # create new students via
 # a form
 post '/students' do
   db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
   redirect '/'
 end
+
+post '/assign_advisor' do 
+  input_id = params["ID"]
+  ID = input_id.to_i
+    if (ID >= 1 && ID <= 10)
+      advisor = "Glenna"
+    elsif (ID >= 11 && ID < 21)
+      advisor = "Andrew"
+    elsif (ID >= 21 && ID < 31)
+      advisor = "Emmanuel"
+    elsif (ID >= 31 && ID < 41)
+      advisor = "Tyler"
+    else
+      advisor = nil
+    end
+
+    if advisor == nil
+      "You've been added to our super fun wait list!"
+    else
+      "Your advisor is #{advisor}"
+    end
+end 
 
 # add static resources
